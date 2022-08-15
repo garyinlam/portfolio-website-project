c14148d8ee22edbe48ce29ad14060408606f717b
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-15 09:44:18 +0100

    overlay works

--- scripts.js
+++ scripts.js
@@ -1,25 +1,12 @@
-// $(document).ready(function(){
-//   $('.card').click(function () {
-//     $('.card__overlay').show();
-//   });
-//   $('.overlay__close').click(function () {
-//     $('.card__overlay').hide();
-//     console.log("clicked")
-//   });
-// });
-
 var openedID;
 
 function showOverlay(id) {
-  openedID = id;
+  openedID = id.parentNode.id + "-overlay";
   var clicked = document.getElementById(openedID);
-  var child = clicked.querySelector('.overlay');
-  child.style.display = "block";
-  child.style.visibility = "visible"
+  clicked.style.display = "block";
 }
 
 function hideOverlay(object) {
   var clicked = document.getElementById(openedID);
-  var child = clicked.querySelector('.overlay');
-  child.removeAttribute("style");
+  clicked.style.display = "none";
 }
