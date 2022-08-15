1e780daa8354e89b40f464318b84468765faeaa2
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-15 14:21:58 +0100

    small changes

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-var openedID;
+let openedID;
 
 function showOverlay(id) {
   openedID = id.parentNode.id + "-overlay";
-  var clicked = document.getElementById(openedID);
+  let clicked = document.getElementById(openedID);
   clicked.style.display = "block";
 }
 
 function hideOverlay(object) {
-  var clicked = document.getElementById(openedID);
+  let clicked = document.getElementById(openedID);
   clicked.style.display = "none";
 }
